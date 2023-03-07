18821ff1332f88775cbc55782d1ae51ded255203
Ashergreen82 <greena@rogers.com>
2023-03-06 21:48:35 -0500

    moved git repository

---
+++ chatapp/client/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Chat from "./Chat";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <header className="App-header">
+        <h1>HI! NEW CHAT APP COMING SOON</h1>
+      </header> */}
+      <div className="container">
+        <Chat />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ chatapp/client/src/Chat.js
@@ -0,0 +1,9 @@
+function Chat() {
+    return (
+        <>
+            <h1>THIS IS THE CHAT PAGE</h1>
+        </>
+    )
+}
+
+export default Chat
