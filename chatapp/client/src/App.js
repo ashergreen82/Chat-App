8ff30f3909fa8dab785f805d1d91c2607de6f4e6
Ashergreen82 <greena@rogers.com>
2023-03-07 07:02:08 -0500

    The earnest work begins!

--- chatapp/client/src/App.js
+++ chatapp/client/src/App.js
@@ -4,9 +4,6 @@ import Chat from "./Chat";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>HI! NEW CHAT APP COMING SOON</h1>
-      </header>
       <div className="container">
         <Chat />
       </div>
--- chatapp/client/src/Chat.js
+++ chatapp/client/src/Chat.js
@@ -1,7 +1,19 @@
 function Chat() {
     return (
         <>
-            <h1>THIS IS THE CHAT PAGE</h1>
+            <h1>SUPER SIMPLE CHAT</h1>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+            <form className="d-flex flex-row justify-content-evenly m-0 p-0">
+                <input className="form-control w-75" type="search" placeholder="Enter your message here" aria-label="Search" />
+                <button className="btn btn-outline-success" type="submit">Search</button>
+            </form>
         </>
     )
 }
