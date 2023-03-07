21da6b42e8ce83e4e6d31ddeb18e3c56d6a4d9d6
Ashergreen82 <greena@rogers.com>
2023-03-07 05:57:43 -0500

    Selected and installed a favicon

--- chatapp/client/src/App.js
+++ chatapp/client/src/App.js
@@ -4,9 +4,9 @@ import Chat from "./Chat";
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
+      <header className="App-header">
         <h1>HI! NEW CHAT APP COMING SOON</h1>
-      </header> */}
+      </header>
       <div className="container">
         <Chat />
       </div>
