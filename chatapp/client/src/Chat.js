efa36278460b33e51a6d5f82e170eefdc9a60650
Ashergreen82 <greena@rogers.com>
2023-03-09 06:59:44 -0500

    Messages box works
    Messages now typed in the input box, now appears in the message box.

--- chatapp/client/src/Chat.js
+++ chatapp/client/src/Chat.js
@@ -1,71 +1,57 @@
+import { useState } from 'react';
+
 function Chat() {
+    const [messages, setMessages] = useState([])
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const messageInput = event.target.elements.message;
+        const newMessage = messageInput.value;
+        setMessages([...messages, newMessage]);
+        messageInput.value = '';
+    };
+
     return (
         <>
-            {/* <h1>SUPER SIMPLE CHAT</h1>
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-8 offset-md-2">
-                        <div className="row">
-                            <div className="col-md-4">
-                                <div className="user-list-box border">
-                                    <h4 className="user-list-heading border">Users</h4>
-                                    <ul>
-                                        <li>User 1</li>
-                                        <li>User 2</li>
-                                        <li>User 3</li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div className="col-md-8">
-                                <div className="row">
-                                    <div className="col-md-12">
-                                        <div className="chat-box border">
-                                            <div className="incoming-message">
-                                                <span className="message-user">User 1:</span> Hello!
-                                            </div>
-                                            <div className="outgoing-message">
-                                                <span className="message-user">You:</span> Hi there!
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <form className="d-flex flex-row justify-content-evenly m-0 p-0">
-                    <input className="form-control w-75" type="search" placeholder="Enter your message here" aria-label="Search" />
-                    <button className="btn btn-outline-success" type="submit">Search</button>
-                </form>
-            </div> */}
             <h1>SUPER SIMPLE CHAT</h1>
             <div className="container">
                 <div className="row">
-                    <div className="col-md-3">
+                    <div className="col-md-2">
                         <div className="user-list-box border">
                             <h4 className="user-list-heading border">Users</h4>
                             <ul className="list-unstyled">
                                 <li>User 1</li>
                                 <li>User 2</li>
                                 <li>User 3</li>
+                                <li>User 4</li>
                             </ul>
                         </div>
                     </div>
-                    <div className="col-md-9">
-                        <div className="row">
+                    <div className="col-md-10">
+                        <div className="row h-100">
                             <div className="col-md-12">
-                                <div className="chat-box border">
-                                    <div className="incoming-message">
-                                        <span className="message-user">User 1:</span> Hello!
-                                    </div>
-                                    <div className="outgoing-message">
-                                        <span className="message-user">You:</span> Hi there!
-                                    </div>
+                                <div className="chat-box border-primary h-100 overflow-auto" style={{ maxHeight: '400px', overflowY: 'auto' }}>
+                                    {messages.map((message, index) => {
+                                        return (
+                                            <div className="outgoing-message" key={index}>
+                                                <span className="message-user">You:</span> {message}
+                                            </div>
+                                        );
+                                    })}
                                 </div>
                             </div>
                             <div className="col-md-12">
-                                <form className="d-flex flex-row justify-content-between align-items-center m-0 p-0 mt-4">
-                                    <input className="form-control w-100" type="search" placeholder="Enter your message here" aria-label="Search" />
+                                <form
+                                    className="d-flex flex-row justify-content-between align-items-center m-0 p-0 mt-4"
+                                    onSubmit={handleSubmit}
+                                >
+                                    <input
+                                        className="form-control w-100"
+                                        type="text"
+                                        placeholder="Enter your message here"
+                                        aria-label="Search"
+                                        name="message"
+                                    />
                                     <button className="btn btn-outline-success" type="submit">Send</button>
                                 </form>
                             </div>
