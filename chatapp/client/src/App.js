51969b32f63f480ac622ccf967d8912ba793ec61
Ashergreen82 <greena@rogers.com>
2023-03-13 11:51:40 -0400

    Added username and login features

--- chatapp/client/src/App.js
+++ chatapp/client/src/App.js
@@ -1,15 +1,91 @@
 import './App.css';
 import Chat from "./Chat";
 import Login from './Login';
+import { useState } from 'react';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [isNewUser, setIsNewUser] = useState(false);
+
+  const handleLogin = (event) => {
+    // event.preventDefault();
+    const storedUser = localStorage.getItem(username);
+    if (storedUser && storedUser.password === password) {
+      handleLogin(username);
+    } else {
+      alert('Invalid username or password');
+    }
+  };
+
+  const handleRegister = (event) => {
+    // event.preventDefault();
+    if (password === confirmPassword) {
+      localStorage.setItem(username, JSON.stringify({ password }));
+      handleRegister({ username, password });
+    } else {
+      alert('Passwords do not match');
+    }
+  };
+
+  // const handleLogin = (username) => {
+  //   setUsername(username);
+  //   setIsLoggedIn(true);
+  // };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    setUsername('');
+  };
+
   return (
     <div className="App">
       <div className="container">
-        <Login />
+        {!isLoggedIn ? (
+          <Login
+            handleLogin={handleLogin}
+            handleRegister={handleRegister}
+            isLoggedIn={isLoggedIn}
+            setIsLoggedIn={setIsLoggedIn}
+            username={username}
+            setUsername={setUsername}
+            password={password}
+            setPassword={setPassword}
+            confirmPassword={confirmPassword}
+            setConfirmPassword={setConfirmPassword}
+            isNewUser={isNewUser}
+            setIsNewUser={setIsNewUser}
+          />
+        ) : (
+          <Chat
+            username={username}
+            handleLogout={handleLogout}
+          />
+        )}
       </div>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+// import './App.css';
+// import Chat from "./Chat";
+// import Login from './Login';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <div className="container">
+//         <Login />
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
--- chatapp/client/src/Login.js
+++ chatapp/client/src/Login.js
@@ -1,28 +1,74 @@
 import { useState } from 'react';
 
-function Login(props) {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+function Login({ handleLogin, handleRegister, isLoggedIn, setIsLoggedIn, username, setUsername, password, setPassword, confirmPassword, setConfirmPassword, isNewUser, setIsNewUser }) {
+    //     const [username, setUsername] = useState('');
+    //     const [password, setPassword] = useState('');
+    //     const [confirmPassword, setConfirmPassword] = useState('');
+    // const [isNewUser, setIsNewUser] = useState(false);
 
-    const handleLogin = (event) => {
-        event.preventDefault();
-        props.handleLogin(username);
+    // const handleLogin = (event) => {
+    //     event.preventDefault();
+    //     const storedUser = localStorage.getItem(username);
+    //     if (storedUser && storedUser.password === password) {
+    //         props.handleLogin(username);
+    //     } else {
+    //         alert('Invalid username or password');
+    //     }
+    // };
+
+    // const handleRegister = (event) => {
+    //     event.preventDefault();
+    //     if (password === confirmPassword) {
+    //         localStorage.setItem(username, JSON.stringify({ password }));
+    //         props.handleRegister({ username, password });
+    //     } else {
+    //         alert('Passwords do not match');
+    //     }
+    // };
+
+    const toggleNewUser = () => {
+        setIsNewUser(!isNewUser);
+        setUsername('');
+        setPassword('');
+        setConfirmPassword('');
     };
 
     return (
-        <form onSubmit={handleLogin}>
-            <div>
-                <label>Username:</label>
-                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-            </div>
-            <div>
-                <label>Password:</label>
-                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            </div>
-            <button type="submit">Login</button>
-        </form>
+        <div>
+            {isNewUser ? (
+                <form onSubmit={handleRegister}>
+                    <div>
+                        <label>New Username:</label>
+                        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+                    </div>
+                    <div>
+                        <label>New Password:</label>
+                        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    </div>
+                    <div>
+                        <label>Confirm Password:</label>
+                        <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+                    </div>
+                    <button type="submit">Register</button>
+                </form>
+            ) : (
+                <form onSubmit={handleLogin}>
+                    <div>
+                        <label>Username:</label>
+                        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+                    </div>
+                    <div>
+                        <label>Password:</label>
+                        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    </div>
+                    <button type="submit">Login</button>
+                </form>
+            )}
+            <button onClick={toggleNewUser}>
+                {isNewUser ? 'Already have an account? Login' : "Don't have an account? Register"}
+            </button>
+        </div>
     );
 }
 
 export default Login;
-
