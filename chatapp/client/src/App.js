ab44119085a499b4a38b225748840725f913483f
Ashergreen82 <greena@rogers.com>
2023-03-20 06:10:13 -0400

    converted everything over to Axios

--- chatapp/client/src/App.js
+++ chatapp/client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Chat from "./Chat";
 import Login from './Login';
 import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -12,41 +13,101 @@ function App() {
 
   const handleLogin = (event) => {
     event.preventDefault();
-    // let storedUser;
-    try {
-      const storedUser = JSON.parse(localStorage.getItem(username));
-      // storedUser = JSON.parse(localStorage.getItem(username));
-      console.log("App.js: storedUser item is ", storedUser);
-      console.log("App.js: password value in handleLogin function is ", password);
-      console.log("username value in App.js (before IF statements) is: ", username);
-      console.log("storedUser.password value in App.js (before IF statements) is: ", storedUser.password);
-      if (storedUser && storedUser.password === password) {
-        setUsername(username);
-        setIsLoggedIn(true);
-        console.log("username value in App.js is: ", username)
-        console.log("Congratulations!!!  You remembered your username and password!!!!")
-      } else {
-        console.log("username value in App.js (else statement) is: ", username)
-        console.log("storedUser value in App.js (else statement) is: ", storedUser)
-        console.log("storedUser.password value in App.js (else statement) is: ", storedUser.password)
+    axios.post('http://localhost:5000/login', {
+      username: username,
+      password: password
+    })
+      .then(response => {
+        if (response.data.success) {
+          setUsername(username);
+          setIsLoggedIn(true);
+          console.log("username value in App.js is: ", username)
+          console.log("Congratulations!!!  You remembered your username and password!!!!")
+        } else {
+          console.log("username value in App.js (else statement) is: ", username)
+          alert('Invalid username or password');
+        }
+      })
+      .catch(error => {
+        console.log(error);
         alert('Invalid username or password');
-      }
-    } catch (event) {
-      alert("Invalid username or password App.js:handleLogin");
-      return;
-    }
+      });
+    // fetch('http://localhost:5000/login', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   },
+    //   mode: 'no-cors',
+    //   body: JSON.stringify({ username, password })
+    // })
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     if (data.success) {
+    //       setUsername(username);
+    //       setIsLoggedIn(true);
+    //       console.log("username value in App.js is: ", username)
+    //       console.log("Congratulations!!!  You remembered your username and password!!!!")
+    //     } else {
+    //       console.log("username value in App.js (else statement) is: ", username)
+    //       alert('Invalid username or password');
+    //     }
+    //   });
+    // let storedUser;
+    // try {
+    //   const storedUser = JSON.parse(localStorage.getItem(username));
+    // storedUser = JSON.parse(localStorage.getItem(username));
+    //   console.log("App.js: storedUser item is ", storedUser);
+    //   console.log("App.js: password value in handleLogin function is ", password);
+    //   console.log("username value in App.js (before IF statements) is: ", username);
+    //   console.log("storedUser.password value in App.js (before IF statements) is: ", storedUser.password);
+    //   if (storedUser && storedUser.password === password) {
+    //     setUsername(username);
+    //     setIsLoggedIn(true);
+    //     console.log("username value in App.js is: ", username)
+    //     console.log("Congratulations!!!  You remembered your username and password!!!!")
+    //   } else {
+    //     console.log("username value in App.js (else statement) is: ", username)
+    //     console.log("storedUser value in App.js (else statement) is: ", storedUser)
+    //     console.log("storedUser.password value in App.js (else statement) is: ", storedUser.password)
+    //     alert('Invalid username or password');
+    //   }
+    // } catch (event) {
+    //   alert("Invalid username or password App.js:handleLogin");
+    //   return;
+    // }
   };
 
   const handleRegister = (event) => {
     event.preventDefault();
     if (password === confirmPassword) {
       localStorage.setItem(username, JSON.stringify({ password }));
-      handleRegister({ username, password });
+      registerUser({ username, password });
     } else {
       alert('Passwords do not match');
     }
   };
 
+  const registerUser = ({ username, password }) => {
+    fetch('http://localhost:5000/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      mode: 'no-cors',
+      body: JSON.stringify({ username, password })
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data.success) {
+          setUsername(username);
+          setIsLoggedIn(true);
+          console.log("username value in App.js is: ", username)
+          console.log("Congratulations!!!  You have successfully registered and logged in!!!!")
+        } else {
+          alert('Registration failed');
+        }
+      });
+  };
   // const handleLogin = (username) => {
   //   setUsername(username);
   //   setIsLoggedIn(true);
