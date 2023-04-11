0f9a234a62824a08868393fa4bfbfd2e93f6c99c
Ashergreen82 <greena@rogers.com>
2023-04-10 21:55:08 -0400

    Removed old commented code no longer needed

--- chatapp/client/src/App.js
+++ chatapp/client/src/App.js
@@ -137,19 +137,3 @@ function App() {
 }
 
 export default App;
-
-// import './App.css';
-// import Chat from "./Chat";
-// import Login from './Login';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <div className="container">
-//         <Login />
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
