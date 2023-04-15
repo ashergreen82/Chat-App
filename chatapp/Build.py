44a2664c1d70fdeb39fc604d8c7327870b58b066
Ashergreen82 <greena@rogers.com>
2023-04-15 14:32:55 -0400

    Another attempt to get the buiild work.

--- chatapp/Build.py
+++ chatapp/Build.py
@@ -2,12 +2,12 @@ import os
 
 
 def setup_server():
-    os.system("pip install -r ./server/requirements.txt")
+    os.system("pip install -r requirements.txt")
 
 
 def setup_client():
-    os.system("npm install --prefix client")
-    os.system("npm run build --prefix client")
+    os.system("npm install --prefix ../client")
+    os.system("npm run build --prefix ../client")
 
 
 if __name__ == "__main__":
